a0710db4317bb207a6ef918e5a705df6acc5539c
history2 <77730885+history2@users.noreply.github.com>
2021-03-15 18:25:25 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir ~/.screen && chmod 700 ~/.screen
 export SCREENDIR=$HOME/.screen
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;32m\]@\[\033[01;34m\]\h:\[\033[01;32m\]\w\[\033[01;32m\]\$\[\033[00;37m\]'
 /usr/bin/ttyd -p $PORT -c admin:adminks123 bash
